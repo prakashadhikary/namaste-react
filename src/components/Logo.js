7eb5f7ee38ba9f4497555301e7fbda802a0885e2
Prakash Adhikari <prakash.adhikari@itonics.de>
2023-12-18 22:39:07 +0545

    Add routing to the logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+import { ROUTE } from "../utils/constants";
+
 // Logo Component
 const Logo = () => {
     return (
         <div id="logo" className="logo">
-            Prakashop
+            <Link to={ROUTE.HOME}>prakaShop</Link>
         </div>
     );
 };
